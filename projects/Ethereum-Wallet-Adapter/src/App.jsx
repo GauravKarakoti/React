ff093fcf2695390e05cec53903ecb9b7c101eae1
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-06-22 15:13:30 +0530

    Fixing Errors

--- projects/Ethereum-Wallet-Adapter/src/App.jsx
+++ projects/Ethereum-Wallet-Adapter/src/App.jsx
@@ -2,13 +2,15 @@ import { createConfig, http, injected, useAccount, useBalance, useConnect, useSe
 import './App.css'
 import { mainnet } from 'viem/chains'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { parseEther } from 'viem';
+import { useState } from 'react';
 
 export const config = createConfig({
   chains: [mainnet],
   connectors: [
     injected()
   ], transports: {
-    [mainnet.id]: http()
+    [mainnet.id]: http('https://eth.drpc.org')
   }
 });
 const queryClient = new QueryClient();
@@ -26,32 +28,39 @@ function App() {
 function MyAddress() {
   const { address } = useAccount();
   // BigInt
-  const balance = useBalance({ address });
-  console.log(balance);
+  const { data, isPending, isError, error } = useBalance({ address });
+  console.log(data);
+  if (error) console.error("Balance Fetch Error:", error.message);
+  if (!address) return <div>Please connect your wallet.</div>;
+  
+  if (isPending) return <div>Loading balance...</div>;
+  
+  if (isError) return <div>Error fetching balance.</div>;
   return <div>
     {address}<br/>
-    {balance?.data?.formatted} ETH
+    {data?.value.toString()} ETH
   </div>
 }
 function WalletConnector() {
   const {connectors, connect} = useConnect();
-  return connectors.map((connector) => {
+  return connectors.map((connector) => (
     <button key={connector.uid} onClick={() => connect({ connector })}>
       {connector.name}
     </button>
-  })
+  ))
 }
 function EthSend() {
   const {data: hash, sendTransaction} = useSendTransaction();
+  const [amount, setAmount] = useState('');
   function sendEth() {
     sendTransaction({
       to: document.getElementById("address").value,
-      value: document.getElementById("amount").value * 1000000000000000000     // 18 0s = 1 ETH
+      value: parseEther(amount)
     })
   }
   return <div>
     <input id="address" type='text' placeholder='Address...'></input>
-    <input id="amount" type='text' placeholder='Amount'></input>
+    <input onChange={(e) => setAmount(e.target.value)} type='text' placeholder='Amount'></input>
     <button onClick={sendEth}>Send ETH</button>
     {hash}
   </div>
